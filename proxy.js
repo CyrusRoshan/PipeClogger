bbf4e4edec92a1740b2d5bba7362c6e441a7ffee
Cyrus Roshan <cyrusroshan123@gmail.com>
2018-01-09 15:23:53 -0600

    Add support for refreshing blocked websites, without requiring extension reload.

--- proxy.js
+++ proxy.js
@@ -1,16 +1,28 @@
-getOptions((options) => {
-    const cloggedURLS = options.cloggedURLS.split(/,\ ?/);
-    const cloggedTime = Number(options.cloggedTime);
-    
-    // clogging urls
-    chrome.webRequest.onBeforeRequest.addListener(
-        (request) => {
-            // must synchronously wait. Unfortunately the API doesn't support async
-            syncWait(cloggedTime);
-    
-            return { cancel: false };
-        },
-        { urls: cloggedURLS },
-        ['requestHeaders', 'blocking']
-    );
+function clogRequest(cloggedURLS, cloggedTime) {
+    return (request) => {
+        syncWait(cloggedTime); // unfortunately the API doesn't allow for async timeouts
+
+        return { cancel: false };
+    }
+}
+
+function runProxy() {
+    getOptions((options) => {    
+        const cloggedURLS = options.cloggedURLS.split(/,\ ?/);
+        const cloggedTime = Number(options.cloggedTime);
+
+        chrome.webRequest.onBeforeRequest.addListener(
+            clogRequest(cloggedURLS, cloggedTime),
+            { urls: cloggedURLS },
+            ['blocking']
+        );
+    })
+}
+
+// Reset proxy with new clogged URLs and clog time
+chrome.storage.onChanged.addListener((options) => {
+    chrome.webRequest.onBeforeRequest.removeListener(clogRequest);
+    runProxy();
 })
+
+runProxy();
